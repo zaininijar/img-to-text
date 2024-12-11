78628cfe2a030b9cddc75b3d0d60e0fd1fc5d2bb
zaininijar <zaininijar7610@gmail.com>
2024-12-12 05:31:11 +0700

    .

---
+++ test.java
@@ -0,0 +1,38 @@
+class Test {
+    // public static void main(String[] args) {
+    // int a = 18;
+    // int b = 33;
+    // int x = 15;
+    // int c = a | b;
+    // boolean d;
+    // d = x < a;
+    // System.out.println(d);
+    // }
+    // public static void main(String[] args) {
+    // int a = 18;
+    // int b = 33;
+    // int c;
+    // c = a % b;
+    // System.out.println(c);
+    // }
+    // public static void main(String[] args) {
+    // int a = 18;
+    // int b = 33;
+    // boolean c;
+    // c = a < b;
+    // System.out.println(c);
+    // }
+    public static void main(String[] args) {
+        int a = 20;
+        int b;
+        b = a >> 4;
+        System.out.println(b);
+    }
+    // public static void main(String[] args) {
+    // boolean a = false;
+    // boolean b = true;
+    // boolean c;
+    // c = a | b;
+    // System.out.println(!c);
+    // }
+}
